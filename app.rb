9c68a7a0511738109f51e5c31d92ee868330f136
onunu <riku.onuma@livesense.co.jp>
2018-03-05 22:19:09 +0900

    provide index page

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require './meyasu_box'
 
 Dotenv.load
 
+get '/' do
+  'Hello, this is slack app. This app does not provide UI'
+end
+
 post '/' do
   return status(200) if MeyasuBox.post(params)
   status(500)
